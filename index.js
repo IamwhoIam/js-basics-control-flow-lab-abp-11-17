ac29fd75d06e2e14bee2b2e7a499e49af521c7b5
Sun-Chow Chan <scchan888@gmail.com>
2017-11-11 05:43:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+scuberGreetingForFeet(distanceTravelled) {
+  if (distanceTravelled <= 400) {
+    return ("This one is on me!")
+  }
+
+  else if (distanceTravelled > 2000) {
+    return("I will gladly take your thirty bucks.")
+  }
+
+  else if (distanceTravelled > 2500) {
+    return("No can do.")
+  }
+}
+scuberGreetingForFeet(199);
